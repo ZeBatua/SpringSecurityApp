bff76b4e86f94cc3588e23826bbf4ab9f9728999
thalassa_home <blinov.gosha@mail.ru>
2023-06-28 15:07:02 +0300

    simple version of config

--- src/main/java/spring/app/config/SecurityConfig.java
+++ src/main/java/spring/app/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package spring.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import spring.app.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import spring.app.services.PersonDetailsServiceImpl;
 
 /**
  * Spring Security Setup, authorization, authentication
@@ -13,16 +16,20 @@ import spring.app.security.AuthProviderImpl;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
-
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
-    }
+    private final PersonDetailsServiceImpl personDetailsService;
 
     @Autowired
+    public SecurityConfig(PersonDetailsServiceImpl personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider((authProvider));
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
