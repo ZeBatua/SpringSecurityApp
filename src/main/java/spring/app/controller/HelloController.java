d877807b40a5a66e728ded8d60850e9d13e2455e
thalassa_home <blinov.gosha@mail.ru>
2023-06-28 14:08:34 +0300

    add userInfo mapping

--- src/main/java/spring/app/controller/HelloController.java
+++ src/main/java/spring/app/controller/HelloController.java
@@ -1,12 +1,23 @@
 package spring.app.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import spring.app.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/getUserInfo")
+    public String info() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
         return "hello";
     }
 }
