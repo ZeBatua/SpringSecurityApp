e398ebd684e00810b3ed17fb148ac8992410d35f
thalassa_home <blinov.gosha@mail.ru>
2023-06-28 15:51:53 +0300

    add custom authorization page

--- src/main/java/spring/app/config/SecurityConfig.java
+++ src/main/java/spring/app/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package spring.app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,6 +27,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        // configure custom login page & authentication (available pages)
+        http.csrf().disable() // отключаем защиту от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
     }
 
     @Bean
